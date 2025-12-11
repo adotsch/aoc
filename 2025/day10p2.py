9015a982bf485449cd7dc9bbb89679a1d3340141
dotsch <dotsch@gmail.com>
2025-12-11 14:16:49 +0100

    day 10, part 2 in Python

--- 2025/day10p2.py
+++ 2025/day10p2.py
@@ -7,11 +7,9 @@ button = [[eval('['+x[1:-1]+']') for x in s[1:-1]] for s in input]
 jolt = [eval('['+s[-1][1:-1]+']') for s in input]
 
 def solve2(bu,jo):
-    cost = np.ones(len(bu))
     constraint = np.zeros((len(jo),len(bu)))
-    target = np.array(jo)
     for i, b in enumerate(bu):
         constraint[b,i] = 1
-    return linprog(cost,A_eq=constraint,b_eq=target,integrality=True).fun
+    return linprog([1]*len(bu),A_eq=constraint,b_eq=jo,integrality=True).fun
 
 print(sum(solve2(x,y) for (x,y) in zip(button,jolt)))
